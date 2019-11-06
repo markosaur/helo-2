fa952fb12563feed2c84aa3036b3a78bde34fe4a
Markosaur <mark3pace@gmail.com>
2019-11-05 22:51:46 -0700

    working on posting and searching

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import {updateUser} from '../../ducks/reducer'
+import {connect} from 'react-redux'
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            search: ''
+
+        }
+    }
+
+    handleChange(e, key) {
+        this.setState({
+          [key]: e.target.value
+        })
+      }
+
     render() {
         return (
             <div>
                 Dashboard
+                <div>
+                    <input
+                    onChange = {e => this.handleChange(e, 'search')}
+                    type= "text"
+                    placeholder = "search"
+                    />
+                </div>
+                <button>Search</button>
+                <button>Reset</button>
             </div>
         )
     }
 }
+
+function mapStateToProps(state) {
+    return {
+        id: state.id,
+        username: state.username,
+        profile_pic: state.profile_pic
+}
+};
+
+export default connect(mapStateToProps, {updateUser})(Dashboard)
