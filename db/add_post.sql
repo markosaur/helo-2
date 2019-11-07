815a97d2207f83b717ee847b56f7e9a090acb9aa
Markosaur <mark3pace@gmail.com>
2019-11-07 00:36:26 -0700

    working on getting posts

---
+++ db/add_post.sql
@@ -0,0 +1,6 @@
+insert into posts
+(title, img, content, author_id)
+values
+( $(title), $(img), $(content), $(author_id) );
+
+select * from posts
--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -7,30 +7,72 @@ class Dashboard extends Component {
     constructor(props){
         super(props)
         this.state = {
-            search: ''
-
+            search: '',
+            posts: [],
+            myPosts: true,
+            isSearch: false
         }
+        this.getPosts = this.getPosts.bind(this)
     }
+
+componentDidMount(){
+    console.log('attempting to mount')
+    this.getPosts()
+}
+
+getPosts(){
+    if(this.state.myPosts === true && this.state.isSearch === false){
+        //need some logic and backend to get all my individual posts need to do a get for one person based on their id
+    }
+    //if myposts === true and is search === true{ do a get with a search according to all these words}
+}
 
     handleChange(e, key) {
         this.setState({
-          [key]: e.target.value
+          [key]: e.target.value,
+          isSearch: !this.state.isSearch
         })
       }
 
+    toggle=()=>{
+        this.setState({
+            myPosts: !this.state.myPosts
+        })
+    }
+
     render() {
+        const mappedPosts = this.state.posts.map((post, i)=>{
+            return(
+                <div>
+                    <p>{post.profile_pic}</p>
+                    <p>{post.username}</p>
+                    <p>{post.profile}</p>
+                </div>
+            )
+        })
+        console.log(this.state)
         return (
             <div>
                 Dashboard
+                
                 <div>
                     <input
                     onChange = {e => this.handleChange(e, 'search')}
                     type= "text"
-                    placeholder = "search"
+                    placeholder = "search"  
+                    />
+                    <input
+                    type = "checkbox"
+                    checked = {this.state.myPosts}
+                    onClick = {() => this.toggle()}
                     />
                 </div>
+
                 <button>Search</button>
                 <button>Reset</button>
+
+                {mappedPosts}
+
             </div>
         )
     }
